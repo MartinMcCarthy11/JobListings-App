b785fa998a1d0f7f2113a7875e01e0d8a88d7e93
martin mccarthy <mccartm6@gmail.com>
2020-05-19 17:17:36 +0100

    Started work on filtering the jobs by selected skills

--- job-listing-app/src/App.js
+++ job-listing-app/src/App.js
@@ -4,17 +4,50 @@ import jobsListData from './data.json';
 import './App.scss';
 
 class App extends Component {
-  state = {jobList:[]}
+  state = {
+    jobList:[],
+    selectedSkills: [],
+    allSkills: []
+  }
+
+  componentWillMount(){
+    this.setState({jobList:jobsListData})
+  }
 
   componentDidMount(){
-    this.setState({jobList:jobsListData})
+    const skills = this.state.jobList.flatMap(o => o.languages).filter(x => x !== undefined);
+    const level = this.state.jobList.flatMap(o => o.role);
+    const role = this.state.jobList.flatMap(o => o.level);
+    const mergedArray =[...role, ...level, ...skills];
+    const allSkills =  [...new Set(mergedArray)];
+    this.setState({allSkills});
+  }
+
+  onSkillSelection = (skill) => {
+        let selectedSkills = [...this.state.selectedSkills];
+        selectedSkills.push(Object.values(skill)[0])
+        selectedSkills = [...new Set(selectedSkills)]
+        this.setState({selectedSkills })
+    }
+
+  filterSkills = (allSkills, filterSkills) =>{
+    return allSkills.filter(f => !filterSkills.includes(f));
+  }
+
+  filterJobs = (filterSkills, jobs) => {
+    return null;
   }
   
   render(){
+    let filterSkills = this.filterSkills(this.state.allSkills, this.state.selectedSkills);
+
+
+    console.log(this.filterJobs(filterSkills,this.state.jobList ));
+
     return (    
       <div className="App">
         <div className="main-container">
-          <List jobList = {this.state.jobList}/>  
+          <List jobList = {this.state.jobList} onSkillSelection = {this.onSkillSelection}/>  
         </div>        
                
       </div>
--- job-listing-app/src/components/List.jsx
+++ job-listing-app/src/components/List.jsx
@@ -2,34 +2,13 @@ import React, { Component } from 'react'
 import ListItem from './ListItem';
 
 class List extends Component {
-    state = {
-        selectedSkills: [],
-        allSkills: []
-    }
-    
-    static getDerivedStateFromProps(props){
-        const skills = props.jobList.flatMap(o => o.languages).filter(x => x !== undefined);
-        const level = props.jobList.flatMap(o => o.role);
-        const role = props.jobList.flatMap(o => o.level);
-        const mergedArray =[...role, ...level, ...skills];
-        const allSkills =  [...new Set(mergedArray)];
-        return {allSkills : allSkills};
-    }
-
-    onSkillSelection = (skill) => {
-        let selectedSkills = [...this.state.selectedSkills];
-        selectedSkills.push(Object.values(skill)[0])
-        selectedSkills = [...new Set(selectedSkills)]
-        this.setState({selectedSkills })
-    }
-    
     render(){
         return (  
         <div>   
             <React.Fragment>
                 <ul>
                     {this.props.jobList.map(job => (
-                        <ListItem key={job.id} jobListSkills = {job} onSkillSelection = {this.onSkillSelection}/> 
+                        <ListItem key={job.id} jobListSkills = {job} onSkillSelection = {this.props.onSkillSelection}/> 
                     ))}                               
                 </ul>
             </React.Fragment>
