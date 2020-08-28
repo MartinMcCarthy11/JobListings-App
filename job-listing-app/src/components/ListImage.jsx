83f4b824f0799fdc0be2f09b1f47856782cb60ad
martin mccarthy <mccartm6@gmail.com>
2020-08-28 22:16:45 +0100

    Got images working bar 1

--- job-listing-app/src/components/ListImage.jsx
+++ job-listing-app/src/components/ListImage.jsx
@@ -5,24 +5,16 @@ import * as logos from '../images';
 export const ListImage = ({listImage}) => {
   // Logic in each of the next two lines should probably be split into helper functions in a util file
   const imageName = listImage.split('/')[2].split('.')[0];
-  console.log(imageName);
-  let imageKey = imageName.charAt(0).toUpperCase() + imageName.slice(1);
-  
+  let imageKey = imageName.charAt(0).toUpperCase() + imageName.slice(1);  
   if(imageKey.includes('-')){
-    let arr = imageKey.split('-');
-    // for (let i = 0; i < arr.length; i++) {
-    //   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-    //       }
-    arr.map(x => x.charAt(0).toUpperCase() + x.slice(1))
-    console.log(arr);
-    arr = arr.filter(item => item !== "The")
-    arr = arr.filter(item => item !== "Co")
+    let arr = imageKey.split('-').map(x => x.charAt(0).toUpperCase() + x.slice(1))
+    arr = arr.filter(item => item !== "The").filter(item => item !== "Co")
     imageKey = arr.join('');
   }
-  console.log(imageKey);
+  //console.log(imageKey);
   // Weird syntax here is to use the string value contained in imageKey as the key, rather than literally 'imageKey'
   const Logo = logos[`${imageKey}`];
-  console.log(logos);
+  //console.log(logos);
   return(
     <div>
     {Logo ? (<Logo />) : null}
